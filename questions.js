2016eea388179d1fc664d9ec51c274ecd3081ee8
Vicky Cumacenko <vicky.c1892@gmail.com>
2019-10-31 18:01:15 +0000

    Add dockerfile and compose

--- questions.js
+++ questions.js
@@ -1,36 +1,36 @@
 exports.welcome = [
-    {
-      type: 'list',
-      name: 'welcome',
-      message:
-        "Hello, would you like to add your own quest or see other people's?",
-      choices: [
-        { name: 'See the quests!', value: 'old' },
-        { name: 'Start my own!', value: 'new' },
-        { name: 'Clear the quests!', value: 'flush' },
-        { name: 'Leave!', value: 'leave' },
-      ],
-    },
-  ];
-  
-  exports.quest = [
-    { name: 'name', message: 'What is your name?' },
-    { name: 'quest', message: 'What is your quest?' },
-    { name: 'colour', message: 'What is your favourite colour?' },
-  ];
-  
-  exports.getExistingQuests = client => {
-    return {
-      type: 'list',
-      name: 'user',
-      message: "Here's all the people who have added their quest:",
-      choices() {
-        return new Promise((res, rej) => {
-          client.keys('*', (err, keys) => {
-            if (err) console.log(err);
-            res(['back', ...keys]);
-          });
+  {
+    type: 'list',
+    name: 'welcome',
+    message:
+      "Hello, would you like to add your own quest or see other people's?",
+    choices: [
+      { name: 'See the quests!', value: 'old' },
+      { name: 'Start my own!', value: 'new' },
+      { name: 'Clear the quests!', value: 'flush' },
+      { name: 'Leave!', value: 'leave' },
+    ],
+  },
+];
+
+exports.quest = [
+  { name: 'name', message: 'What is your name?' },
+  { name: 'quest', message: 'What is your quest?' },
+  { name: 'colour', message: 'What is your favourite colour?' },
+];
+
+exports.getExistingQuests = client => {
+  return {
+    type: 'list',
+    name: 'user',
+    message: "Here's all the people who have added their quest:",
+    choices() {
+      return new Promise((res, rej) => {
+        client.keys('*', (err, keys) => {
+          if (err) console.log(err);
+          res(['back', ...keys]);
         });
-      },
-    };
+      });
+    },
   };
+};
